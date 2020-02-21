3c0b836abcef309ebb5ef252f321e9320840797f
conye9980 <conorkelly86@gmail.com>
2020-02-21 14:07:17 +0000

    Initial Modal work

--- js/modify.js
+++ js/modify.js
@@ -39,10 +39,37 @@ $(function () {
     $("#amount-avc").val($("#avc-contribution").slider("value") + "%");
 });
 
-var block = document.getElementById('btnSubmit');
+var block = document.getElementById('modal-btn-yes');
 
 block.addEventListener('click', function () {
 
     if (block.getAttribute('class') == 'avc') block.setAttribute('class', 'avc-reordered');
     else block.setAttribute('class', 'avc');
+});
+
+var modalConfirm = function (callback) {
+
+    $("#btn-confirm").on("click", function () {
+        $("#my-modal").modal('show');
+    });
+
+    $("#modal-btn-yes").on("click", function () {
+        callback(true);
+        $("#my-modal").modal('hide');
+    });
+
+    $("#modal-btn-no").on("click", function () {
+        callback(false);
+        $("#my-modal").modal('hide');
+    });
+};
+
+modalConfirm(function (confirm) {
+    if (confirm) {
+        //Acciones si el usuario confirma
+        $("#result").html("CONFIRMED");
+    } else {
+        //Acciones si el usuario no confirma
+        $("#result").html("NOT CONFIRMED");
+    }
 });
